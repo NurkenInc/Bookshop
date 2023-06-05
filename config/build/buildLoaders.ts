9c68b460e4fd4a29fb064a40560cd71933edf670
Nurken <94748525+NurkenInc@users.noreply.github.com>
2023-06-05 12:55:24 +0600

    added file & svg loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,10 +4,10 @@ import { BuildOptions } from './types/config';
 
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-    use: 'ts-loader',
-    exclude: /node_modules/,
+  
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
   }
 
   const cssLoaders = {
@@ -26,8 +26,25 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
       "sass-loader",
     ],
   }
+
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  }
+
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  }
   
   return [
+    fileLoader,
+    svgLoader,
     typescriptLoader,
     cssLoaders,
   ]
