b279d49784d954f03ee969350a7eefbb4f8d2cf5
Nurken <94748525+NurkenInc@users.noreply.github.com>
2023-06-07 23:47:58 +0600

    fix storybook

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,9 +1,20 @@
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { DecoratorFn } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 
-export const StoreDecorator = (initialState: DeepPartial<StateSchema>): DecoratorFn => (StoryComponent) => (
-  <StoreProvider initialState={initialState}>
+const defaultAsyncReducers: DeepPartial<ReducersMapObject<StateSchema>> = {
+  loginForm: loginReducer,
+};
+
+export const StoreDecorator = (
+  initialState: DeepPartial<StateSchema>,
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>,
+): DecoratorFn => (StoryComponent) => (
+  <StoreProvider
+    initialState={initialState}
+    asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}
+  >
     <StoryComponent />
   </StoreProvider>
 );
