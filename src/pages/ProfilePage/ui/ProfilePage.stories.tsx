bdacab574b1f3e7207fd50261e7dd7b0cbb2c825
Nurken <94748525+NurkenInc@users.noreply.github.com>
2023-06-08 12:04:08 +0600

    fixed storybook

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import ProfilePage from './ProfilePage';
 
 const meta: Meta<typeof ProfilePage> = {
@@ -16,6 +17,7 @@ export default meta;
 type Story = StoryObj<typeof ProfilePage>;
 
 export const Normal: Story = {};
+Normal.decorators = [StoreDecorator({})];
 
 export const Dark: Story = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
