31e84f4a9a775d3e9cf831dfefc15f9336d3b3a4
Nurken <94748525+NurkenInc@users.noreply.github.com>
2023-06-11 22:51:22 +0600

    added types saving fixed data fetching on change type

--- src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
+++ src/pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from 'app/providers/StoreProvider';
 import { SortOrder } from 'shared/types';
-import { ArticleSortField } from 'entities/Article';
+import { ArticleSortField, ArticleType } from 'entities/Article';
 import { getArticlesPageInited } from '../../selectors/articlesPageSelector';
 import { articlesPageActions } from '../../slice/articlesPageSlice';
 import { fetchArticlesList } from '../fetchArticlesList/fetchArticlesList';
@@ -20,6 +20,7 @@ export const initArticlesPage = createAsyncThunk<void, URLSearchParams, ThunkCon
       const orderFromUrl = searchParams.get('order') as SortOrder;
       const sortFromUrl = searchParams.get('order') as ArticleSortField;
       const searchFromUrl = searchParams.get('search');
+      const typeFromUrl = searchParams.get('type') as ArticleType;
 
       if (orderFromUrl) {
         articlesPageActions.setOrder(orderFromUrl);
@@ -31,6 +32,10 @@ export const initArticlesPage = createAsyncThunk<void, URLSearchParams, ThunkCon
 
       if (searchFromUrl) {
         articlesPageActions.setSearch(searchFromUrl);
+      }
+
+      if (typeFromUrl) {
+        articlesPageActions.setType(typeFromUrl);
       }
 
       dispatch(articlesPageActions.initState());
